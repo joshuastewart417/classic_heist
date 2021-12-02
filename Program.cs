da71842ab681752f6948a1a209768eac5d269c81
joshuastewart417 <joshua.stewart780@gmail.com>
2021-12-02 10:58:33 -0600

    created rolodex and ability for user to choose crew for heist

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace heist
 {
@@ -23,43 +24,102 @@ namespace heist
             Console.WriteLine("Current Operatives:\n");
             foreach(IRobber robber in rolodex)
             {
-                Console.WriteLine("${robber.Name}");
+                Console.WriteLine($"{robber.Name}");
             }
-            Console.WriteLine("Enter a new crew member:");
-            string crewMember = Console.ReadLine();
-            Console.Write($"What specialty (Hacker, Muscle, Lock Specialist) should {crewMember} have?");
-            string specialty = Console.ReadLine();
-            Console.Write($"What skill level (1-100) should this {specialty} have?");
-            int skillLevel = int.Parse(Console.ReadLine());
-            Console.Write($"What does {crewMember} want as a percentage cut?");
-            int percentageCut = int.Parse(Console.ReadLine());
-            if (specialty == "Hacker")
+
+            string crewMember = "random";
+
+            while (crewMember != "")
             {
-                rolodex.Add(new Hacker()
+
+                Console.WriteLine("Enter a new crew member:");
+                crewMember = Console.ReadLine();
+
+                if(crewMember == "") {
+                    continue;
+                }
+
+                Console.Write($"What specialty should {crewMember} have?");
+                Console.Write("(- Hacker - Muscle -Lock Specialist)");
+                string specialty = Console.ReadLine();
+
+                Console.Write($"What skill level (1-100) should this {specialty} have?");
+                int skillLevel = int.Parse(Console.ReadLine());
+
+                Console.Write($"What does {crewMember} want as a percentage cut?");
+                int percentageCut = int.Parse(Console.ReadLine());
+
+                if (specialty == "Hacker")
                 {
-                    Name = crewMember,
-                    SkillLevel = skillLevel,
-                    PercentageCut = percentageCut
-                });
-            }
-            else if (specialty == "Muscle")
+                    rolodex.Add(new Hacker()
+                    {
+                        Name = crewMember,
+                        SkillLevel = skillLevel,
+                        PercentageCut = percentageCut
+                    });
+
+                    break;
+                }
+                else if (specialty == "Muscle")
+                {
+                    rolodex.Add(new Muscle()
+                    {
+                        Name = crewMember,
+                        SkillLevel = skillLevel,
+                        PercentageCut = percentageCut
+                    });
+
+                    break;
+                }
+                else if (specialty == "Lock Specialist")
+                {
+                    rolodex.Add(new LockSpecialist()
+                    {
+                        Name = crewMember,
+                        SkillLevel = skillLevel,
+                        PercentageCut = percentageCut
+                    });
+
+                    break;
+                }
+            }   
+
+            Random randInt = new Random();
+
+            Bank bank1 = new Bank()
             {
-                rolodex.Add(new Muscle()
-                {
-                    Name = crewMember,
-                    SkillLevel = skillLevel,
-                    PercentageCut = percentageCut
-                });
-            }
-            else if (specialty == "Lock Specialist")
+                AlarmScore = randInt.Next(0, 100),
+                VaultScore = randInt.Next(0, 100),
+                SecurityGuardScore = randInt.Next(0, 100),
+                CashOnHand = randInt.Next(50000, 1000000)
+            };
+
+            Dictionary<string, int> systemList = new Dictionary<string, int>(){
+                {"Alarm", bank1.AlarmScore},
+                {"Vault", bank1.VaultScore},
+                {"Security Guard", bank1.SecurityGuardScore}
+            };
+
+            var sortedDict = from entry in systemList orderby entry. Value ascending select entry;
+
+
+
+
+            Console.WriteLine($"Least Secure: {sortedDict.ElementAt(0).Key}");
+            Console.WriteLine($"Most Secure: {sortedDict.ElementAt(2).Key}");
+
+            for (int i = 0; i < rolodex.Count; i++)
             {
-                rolodex.Add(new LockSpecialist()
-                {
-                    Name = crewMember,
-                    SkillLevel = skillLevel,
-                    PercentageCut = percentageCut
-                });
+                Console.WriteLine($"{i}. {rolodex[i].Name}:  {rolodex[i].SkillLevel} {rolodex[i].PercentageCut}%");
+                Console.WriteLine($"    Speciality: {rolodex[i].GetType().ToString().Split('.')[1]}");
+                Console.WriteLine($"    Skill Level: {rolodex[i].SkillLevel}");
+                Console.WriteLine($"    Skill Level: {rolodex[i].PercentageCut}%");
             }
+
+            List<IRobber> crew = new List<IRobber>();
+            Console.WriteLine("Enter the number of the operative you want to include in the heist");
+            int num = int.Parse(Console.ReadLine());
+            crew.Add(rolodex[num]); 
         }
     }
 }
